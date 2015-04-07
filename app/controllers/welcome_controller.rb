d8d29ead809c096146a7fce6f1b561fc10583250
Lijun Xue <xljroy@gmail.com>
2015-04-07 15:50:19 +0900

    update login part

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    if current_user
+      redirect_to user_path(current_user)
+    elsif current_official
+      redirect_to official_path(current_official)
+    end
   end
 end
