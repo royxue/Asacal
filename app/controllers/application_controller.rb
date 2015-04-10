e0cc51a14629cef1d67282bf182537021e318b3e
Lijun Xue <xljroy@gmail.com>
2015-04-10 12:15:06 +0900

    Update controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User)
+      user_path(resource)
+    elsif resource.is_a?(Official)
+      official_path(resource)
+    end
+  end
 
+  protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :description, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :description, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:provider, :uid, :name, :email, :password, :image) }
   end
 
 end
--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,6 @@
 class CalendarsController < ApplicationController
   before_action :set_calendar, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_official!, only: [:new, :edit, :create, :update, :destroy]
 
   respond_to :html
 
@@ -11,37 +12,37 @@ class CalendarsController < ApplicationController
   def show
     respond_with(@calendar)
   end
-  #
-  # def new
-  #   @calendar = Calendar.new
-  #   respond_with(@calendar)
-  # end
-  #
-  # def edit
-  # end
-  #
-  # def create
-  #   @calendar = Calendar.new(calendar_params)
-  #   @calendar.save
-  #   respond_with(@calendar)
-  # end
-  #
-  # def update
-  #   @calendar.update(calendar_params)
-  #   respond_with(@calendar)
-  # end
-  #
-  # def destroy
-  #   @calendar.destroy
-  #   respond_with(@calendar)
-  # end
-  #
-  # private
-  #   def set_calendar
-  #     @calendar = Calendar.find(params[:id])
-  #   end
-  #
-  #   def calendar_params
-  #     params[:calendar]
-  #   end
+
+  def new
+    @calendar = Calendar.new
+    respond_with(@calendar)
+  end
+
+  def edit
+  end
+
+  def create
+    @calendar = Calendar.new(calendar_params)
+    @calendar.save
+    respond_with(@calendar)
+  end
+
+  def update
+    @calendar.update(calendar_params)
+    respond_with(@calendar)
+  end
+
+  def destroy
+    @calendar.destroy
+    respond_with(@calendar)
+  end
+
+  private
+    def set_calendar
+      @calendar = Calendar.find(params[:id])
+    end
+
+    def calendar_params
+      params[:calendar]
+    end
 end
