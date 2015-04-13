26fbf2e346b6260911727cb46bce801d724f41f3
Lijun Xue <xljroy@gmail.com>
2015-04-13 12:27:24 +0900

    icon file and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :officials
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'registrations'}
 
   get 'welcome/index'
 
